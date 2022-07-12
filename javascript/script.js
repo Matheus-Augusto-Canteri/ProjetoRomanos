47ecda04bd7a7fe84c6d6846e79930e8970f90cf
vinicius-af <91281024+vinicius-af@users.noreply.github.com>
2022-07-12 16:35:11 -0300

    tudo

--- javascript/script.js
+++ javascript/script.js
@@ -94,3 +94,43 @@ document.getElementById("dec").addEventListener('keyup', function() {
     document.getElementById("prova-real").href = provaReal;
     document.getElementById("prova-real").innerHTML  = 'Clique aqui para conferir: ' + numberCheck;
 });
+
+document.getElementById("romano").addEventListener('keyup', function() {		
+    var romanNumber = $("#romano").val();
+        
+        
+    var provasReal = 'http://numeracaoromana.babuo.com/'+ romanNumber + '-numero-romano';
+    document.getElementById("provasRealBox").style = "display: block";
+    document.getElementById("provas-real").href = provasReal;
+    document.getElementById("provas-real").innerHTML  = 'Clique aqui para conferir: ' + romanNumber;
+});
+
+
+
+function retornaRomano(numero) {
+    let resultado = 0;
+    let letnum = null;
+    let algarismos = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000 }
+    numero = numero.toUpperCase();
+    for (let i = numero.length-1; i >= 0; i--) {
+        let char = numero.charAt(i);
+        for (let chave in algarismos) {
+            if( char === chave){
+                let chavenumero = parseInt(algarismos[chave]);
+                if (letnum !== null) {
+                    if (chavenumero < letnum) {
+                        chavenumero = chavenumero*-1;
+                    }
+                }
+                resultado = resultado + chavenumero;
+                letnum = chavenumero;
+            }
+        }
+    }
+    return resultado
+} 
+
+function retornaRomanos(){
+    let number = $('#romano').val();
+    $('#dec').val(retornaRomano(number));
+}
